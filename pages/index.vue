be6d59e1eb7f020e513c1a0427ce185a029d7713
Sushil <iamsushil303@gmail.com>
2021-01-04 16:25:58 +0530

    Routing Check

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-card height="600" class="d-flex justify-center align-center">
     <div>
       <h1 class="goldman">This is my hero</h1>
-      <v-btn href="/projects">Project Page</v-btn>
+      <v-btn block nuxt class="mt-5" to="/projects">Project Page</v-btn>
     </div>
   </v-card>
 </template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -2,7 +2,7 @@
   <v-card height="600" class="d-flex justify-center align-center">
     <div>
       <h1 class="goldman">This is the project page</h1>
-      <v-btn href="/">Main Page</v-btn>
+      <v-btn block nuxt class="mt-5" to="/">Main Page</v-btn>
     </div>
   </v-card>
 </template>
