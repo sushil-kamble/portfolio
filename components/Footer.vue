490d4b6c6aaa4a5a44fe3b643967b5b0d6d115ea
Sushil <iamsushil303@gmail.com>
2021-04-06 23:10:21 +0530

    footer bug fix

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,15 @@
 <template>
   <v-footer color="primary lighten-1" padless>
     <v-row justify="center" no-gutters dense>
-      <div class="py-3 flex-grow-1 text-center white">
+      <div
+        :class="`py-3 flex-grow-1 text-center ${
+          !$vuetify.theme.dark ? 'white' : 'primary darken-3'
+        }`"
+      >
         <v-divider class="mb-3"></v-divider>
         <Icons />
       </div>
-      <v-col class="primary text-center white--text" cols="12">
+      <v-col class="primary darken-2 text-center white--text" cols="12">
         {{ new Date().getFullYear() }} —
         <strong>
           <a
